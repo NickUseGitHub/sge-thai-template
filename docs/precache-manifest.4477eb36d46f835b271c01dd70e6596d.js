56fe20891c50e2e8a83f03f4c3db047ba64f8e90
Chaiwtun <chaiwtun@scg.com>
2020-07-12 09:06:49 +0700

    [master] prepare to deploy github page

---
+++ docs/precache-manifest.4477eb36d46f835b271c01dd70e6596d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0903b65905df79661ae9bdedafe2ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309add6c27d490d786b0",
+    "url": "/static/css/main.3b620ea9.chunk.css"
+  },
+  {
+    "revision": "76bdd9e8ec5ddc40dfae",
+    "url": "/static/js/2.c53a6bb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c53a6bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309add6c27d490d786b0",
+    "url": "/static/js/main.9180d9d4.chunk.js"
+  },
+  {
+    "revision": "bf23c726bc64efa6e752",
+    "url": "/static/js/runtime-main.f216ec27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
