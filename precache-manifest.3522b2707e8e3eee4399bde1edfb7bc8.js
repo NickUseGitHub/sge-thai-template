d5b5dc4d99e8114e7305770df8a2193083887dbb
Chaiwtun <chaiwtun@scg.com>
2020-07-15 18:22:33 +0700

    Updates

--- precache-manifest.3522b2707e8e3eee4399bde1edfb7bc8.js
+++ precache-manifest.3522b2707e8e3eee4399bde1edfb7bc8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5924d25390d6ebf193bd2c5583fa2a22",
+    "revision": "e8f9da3701c69da7bb6911df68bca46f",
     "url": "/sge-thai-template/index.html"
   },
   {
-    "revision": "f1714207db89cd2190f9",
+    "revision": "b1421938c0b208d91b71",
     "url": "/sge-thai-template/static/css/main.d821567a.chunk.css"
   },
   {
-    "revision": "3c06b860d5664de7d749",
-    "url": "/sge-thai-template/static/js/2.9e9358b2.chunk.js"
+    "revision": "a9da2b221bac684e17bb",
+    "url": "/sge-thai-template/static/js/2.1e09b23b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/sge-thai-template/static/js/2.9e9358b2.chunk.js.LICENSE.txt"
+    "url": "/sge-thai-template/static/js/2.1e09b23b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f1714207db89cd2190f9",
-    "url": "/sge-thai-template/static/js/main.36c75a9f.chunk.js"
+    "revision": "b1421938c0b208d91b71",
+    "url": "/sge-thai-template/static/js/main.982727de.chunk.js"
   },
   {
     "revision": "c186610965ef1ecea7f3",
