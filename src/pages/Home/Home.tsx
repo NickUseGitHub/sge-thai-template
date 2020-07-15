43260ddf857b918abc1316d050676dcfc113fdd6
Chaiwtun <chaiwtun@scg.com>
2020-07-15 22:56:59 +0700

    [homepage] add Promotions on homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,13 +3,15 @@ import Banner from '../../components/Banner'
 import HomeLineSection from './HomeLineSection'
 import ProductListSection from './ProductListSection'
 import WhyUseUs from './WhyUseUs'
+import PromotionList from './PromotionList'
 
 export default function Home() {
   return (
     <div className="m-auto antialiased font-sans font-serif font-mono text-center">
       <Banner />
       <ProductListSection title="Flash sale!!" isFlashSale />
-      <ProductListSection title="สินค้ายอดนิยม" />
+      <ProductListSection title="Best selling" />
+      <PromotionList />
       <WhyUseUs />
       <HomeLineSection />
     </div>
