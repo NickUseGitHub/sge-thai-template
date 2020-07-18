4a4e8f65bfbd7af80e766f6c19c7ef85f701e274
Chaiwtun <chaiwtun@scg.com>
2020-07-19 00:01:49 +0700

    [master] add CartItems

---
+++ src/components/Cart/CartItem.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+interface CartItemProps {
+  title: string
+  thumbnail: string
+  amount?: number
+}
+
+export default function CartItem({
+  title,
+  thumbnail,
+  amount = 9,
+}: CartItemProps) {
+  return (
+    <div className="w-full flex flex-col md:flex-row md:items-stretch my-3 border-b-2 p-2">
+      <div className="inline-flex w-full md:w-1/4 justify-center mb-2 md:mb-0">
+        <img src={thumbnail} />
+      </div>
+      <div className="inline-flex flex-col justify-between w-full md:w-3/4">
+        <div className="flex flex-col items-start">
+          <span className="text-left text-xl">{title}</span>
+          <span className="pt-1 text-gray-900">£9.99</span>
+        </div>
+
+        <div className="w-full flex items-center">
+          <div className="w-full flex justify-start items-center">
+            {/* Amount controll */}
+            <div className="w-full inline-flex md:w-1/3 border-2 rounded-l-full rounded-r-full overflow-x-hidden mr-2">
+              <button className="w-1/3 bg-green-300 border-r-2 text-center">
+                <i className="fa fa-caret-left" aria-hidden="true"></i>
+              </button>
+              <input
+                className="w-1/3 text-right px-2"
+                type="text"
+                value={amount}
+              />
+              <button className="w-1/3 bg-green-300 border-l-2 text-center">
+                <i className="fa fa-caret-right" aria-hidden="true"></i>
+              </button>
+            </div>
+            {/* Amount controll */}
+            ชิ้น
+          </div>
+
+          <button className="w-full md:w-1/3 text-white rounded-full bg-red-900 mt-1 p-2">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import { mockProducts } from '../configs'
+import CartItem from '../components/Cart/CartItem'
 
 export default function ProductsPage() {
   return (
     <div className="m-auto antialiased font-sans font-serif font-mono text-center">
       <Banner />
-      <div>Cart Page</div>
+      <section className="bg-white py-8">
+        <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+          <h1 className="w-full text-3xl text-left underline px-2 md:px-0">
+            ตะกร้าสินค้า
+          </h1>
+
+          <div className="w-full flex">
+            {/* Item List */}
+            <div className="w-full md:w-3/4 p-2 mt-10">
+              {mockProducts.slice(0, 3).map((product) => (
+                <CartItem
+                  key={product.title}
+                  title={product.title}
+                  thumbnail={product.thumbnail}
+                />
+              ))}
+            </div>
+            {/* End Item List */}
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
