69423f9f387bb580c3c58b641276f82084b7ecaa
Chaiwtun <chaiwtun@scg.com>
2020-07-28 23:12:00 +0700

    Updates

---
+++ precache-manifest.158da9cb8e7d0cae17f563cb8cf35479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2a355cb335de52af413a46eda1ce82",
+    "url": "/sge-thai-template/index.html"
+  },
+  {
+    "revision": "723f8b379fd93f2a7920",
+    "url": "/sge-thai-template/static/css/main.189c7784.chunk.css"
+  },
+  {
+    "revision": "750673b0d710bc48bf9d",
+    "url": "/sge-thai-template/static/js/2.5d096c48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sge-thai-template/static/js/2.5d096c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723f8b379fd93f2a7920",
+    "url": "/sge-thai-template/static/js/main.8581fb95.chunk.js"
+  },
+  {
+    "revision": "c186610965ef1ecea7f3",
+    "url": "/sge-thai-template/static/js/runtime-main.9164da38.js"
+  }
+]);
